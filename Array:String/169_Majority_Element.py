a4902e1659f1e30b320956b0826616b913e53286
Konark Yadav <k.yadav2704@gmail.com>
2024-01-07 17:14:21 -0500

    Update 169_Majority_Element.py

--- Array:String/169_Majority_Element.py
+++ Array:String/169_Majority_Element.py
@@ -1,9 +1,10 @@
 # Reference: https://www.youtube.com/watch?v=7pnhv842keE&ab_channel=NeetCode
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
-
+        # nums.sort()
+        # return nums[len(nums)//2]
+        return Counter(nums).most_common(1)[0][0]
         # set_nums = set(nums)
 
         # for val in set_nums:
