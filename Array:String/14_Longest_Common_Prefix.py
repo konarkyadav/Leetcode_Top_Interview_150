977e17b4f2134c6a123614b2f58b6790b1300878
Konark Yadav <k.yadav2704@gmail.com>
2024-01-09 13:22:33 -0500

    Add files via upload

---
+++ Array:String/14_Longest_Common_Prefix.py
@@ -0,0 +1,11 @@
+# Reference: https://www.youtube.com/watch?v=0sWShKIJoo4&ab_channel=NeetCode
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        
+        return res
